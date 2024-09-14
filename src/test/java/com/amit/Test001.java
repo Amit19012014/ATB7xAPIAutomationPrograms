51d993c426bed6502bc1714bc1df8bc304115f4e
Amit19012014 <Amit19012014@gmail.com>
2024-09-15 00:13:05 +0530

    2 files are add

--- src/test/java/com/amit/Test001.java
+++ src/test/java/com/amit/Test001.java
@@ -1,2 +1,9 @@
-package com.amit;public class Test001 {
+package com.amit;
+
+public class Test001 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+    }
 }
